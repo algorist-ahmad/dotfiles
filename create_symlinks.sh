8729338d50bfbca20503ab6409d245d3654b38c0
Bytesmith Ahmad <b-9k@outlook.com>
2024-05-24 16:29:16 -0400

    Added jrnl settings as well as symlink

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,8 +3,7 @@
 #config
 path="$HOME/rc-files"
 micro='.config/micro/settings.json'
-
-# version 2
+jrnl='.config/jrnl/jrnl.yaml'
 
 symlink "$HOME/.aliases"   to "$path/aliases"
 symlink "$HOME/.bashrc"    to "$path/bashrc"
@@ -15,14 +14,4 @@ symlink "$HOME/.sqliterc"  to "$path/sqliterc"
 symlink "$HOME/.taskrc"    to "$path/taskrc"
 symlink "$HOME/.variables" to "$path/variables"
 symlink "$HOME/$micro"     to "$path/micro_settings.json"
-
-# old version
-
-# ln -s "$path/aliases"             "$HOME/.aliases"
-# ln -s "$path/bashrc"              "$HOME/.bashrc"
-# ln -s "$path/billrc"              "$HOME/.billrc"
-# ln -s "$path/scimrc"              "$HOME/.scimrc"
-# ln -s "$path/sqliterc"            "$HOME/.sqliterc"
-# ln -s "$path/taskrc"			  "$HOME/.taskrc"
-# ln -s "$path/variables"           "$HOME/.variables"
-# ln -s "$path/micro_settings.json" "$HOME/.config/micro/settings.json"
+symlink "$HOME/$jrnl"      to "$path/rc-files/jrnl.yaml"
