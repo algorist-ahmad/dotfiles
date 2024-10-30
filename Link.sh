aa1e418af36a1aa0d0e7345e1f79d67c4ef7372c
ahmad <b-9k@outlook.com>
2024-10-30 15:41:30 -0400

    Added xfce4 to family of configs, and modified Link script to automatically import settings

--- Link.sh
+++ Link.sh
@@ -25,4 +25,8 @@ while IFS=' =' read -r target source; do
     sleep 0.1
 done < "$mapping_file"
 
-echo "Symlink creation completed."
+echo -e "Symlink creation completed.\n"
+
+cp -vi "$home/xfce4/keyboard.shortcuts.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
+
+echo -e "\ncopied xfc4 settings to ~/.config/xfce4 cuz they can't be symlinked"
