015a562ed3974afe4b833d4232307280b36ad5ce
ahmad <b-9k@outlook.com>
2024-12-31 09:42:06 -0500

    expanded list of essential tools

--- setup/essential.sh
+++ setup/essential.sh
@@ -2,8 +2,10 @@
 
 declare -A cmds=(
   [terminal]=kitty
+  [task_manager]=task
   [pass_manager]=pass
   [clipboard]=xclip
+  [email_client]=neomutt
   [editor]=micro
   [file_reader]=bat
   [github]=github-cli
